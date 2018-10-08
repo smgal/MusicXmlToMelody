8eb7a39921b6887e3e2527207486f1cd3d26bd4c
Youngki Ahn <avej.com@gmail.com>
2018-10-08 17:37:09 +0900

    Add XML resource and apply SAX module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MusicXmlToMelody
 Convert a MusicXML file to the main melody and chords information
+
+Sample MusicXML from:
+`https://freemidi.org/download2-11696-gemini-dream-moody-blues`
--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var sax    = require('sax')
 var events = require('events')
 var util   = require('util')
 
+const KEY_NAME = '#name';
+const KEY_ATTR = '#attr';
+
+function Chord()
+{
+}
+
+function Note()
+{
+}
+
 function Measure()
 {
 	this.index  = 0;
@@ -30,6 +41,7 @@ function MusicXmlParser(settings)
 	this.measures = [];
 
 	// Private members
+	this.stack = [];
 	this.measure_obj    = new Measure();
 
 	// Methods
@@ -42,17 +54,69 @@ function MusicXmlParser(settings)
 
 util.inherits(MusicXmlParser, events.EventEmitter);
 
-MusicXmlParser.prototype.OnOpen = function(error)
+MusicXmlParser.prototype.OnOpen = function(node)
 {
+	var tag_name = node.name.toLowerCase();
+	
+	// TODO: process tag_name
+
+	var entity = {};
+
+	entity[KEY_NAME] = tag_name;
+
+	for (var key in node.attributes)
+	{
+		// push the attributes to 'entity'
+		if (!(KEY_ATTR in entity))
+			entity[KEY_ATTR] = {};
+
+		entity[KEY_ATTR][key] = node.attributes[key];
+
+		// TODO: process XML tag
+	}
+  
+	this.stack.push(entity);
+};
+
+MusicXmlParser.prototype.OnClose = function(node)
+{
+	var entity = this.stack.pop();
+	var key = entity[KEY_NAME];
+	var tag_name = node.toLowerCase();
+	  
+	// TODO: process tag_name
+
+	delete entity[KEY_NAME];
+
+	var parent = this.stack[this.stack.length - 1];
+
+	if (entity && (typeof entity === 'object') && (Object.keys(entity).length == 0))
+		entity = true;
+
+	if (this.stack.length > 0)
+	{
+		if (key in parent)
+		{
+			parent[key] = util.isArray(parent[key]) ? parent[key] : [parent[key]];
+			parent[key].push(entity);
+		}
+		else
+		{
+			parent[key] = entity;
+		}
+	}
+	else
+	{
+		var result = {};
+		result[key] = entity;
+
+		this.emit('end', this.measures, result);
+	}	
+
 	// TODO:
 };
 
-MusicXmlParser.prototype.OnClose = function(error)
-{
-	// TODO:
-};
-
-MusicXmlParser.prototype.OnData = function(error)
+MusicXmlParser.prototype.OnData = function(context)
 {
 	// TODO:
 };
@@ -74,18 +138,8 @@ MusicXmlParser.prototype.parse = function(string, internal_callback, user_callba
 	this.sax.write(string);
 };
 
-function ScoreDescriptor1_CB(err, measures, sax_output, user_callback)
+function ScoreDescriptor1_CB(error, measures, sax_output, user_callback)
 {
-	if (error)
-	{
-		user_callback(error, sax_output);
-		return;
-	}
-}
-
-function ScoreDescriptor2_CB(error, measures, sax_output, user_callback)
-{
-	// TODO: TBD
 	if (error)
 	{
 		user_callback(error, sax_output);
@@ -99,6 +153,19 @@ function ScoreDescriptor2_CB(error, measures, sax_output, user_callback)
 		var measure = measures[ix];
 
 		// TODO:
+	}
+
+	// for test
+	user_callback(null, sax_output);
+}
+
+function ScoreDescriptor2_CB(error, measures, sax_output, user_callback)
+{
+	// TODO: TBD
+	if (error)
+	{
+		user_callback(error, sax_output);
+		return;
 	}
 }
 
